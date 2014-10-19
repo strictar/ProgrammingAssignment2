1c85c371ef08a4d585cc9f19fe552e78b965ca21
strictar <lari.strictar@gmail.com>
2014-10-19 16:09:29 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+?matrix
+?solve
 
-## Write a short comment describing this function
+#
+#makeCacheMatrix creates a matrix containing the values of a quadratic matrix with the created values and its inversions (with set and get)
+#cachematrix generates the inversion of the CacheMatrix, but firts it checks 
+# if there is no previous calculation of the asked matrix
+#if it was not calculated it calculates and saves the inverse in the cache
+#but if the inverse was already generated before it just returns the previous values (only if the matrix was not changed) 
+#função setmatrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x=matrix ()){
+  m <-NULL
+  set<- function (y) {
+    x<<-y
+    m<<-NULL
+  }
+  get <- function ()x
+  setmatrix <- function (solve) m<<-solve
+  getmatrix <- function () m
+  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachematrix <- function (x=matrix) {
+  m <- x&getmatrix ()
+  if (!is.null (m)){
+    message ("getting cached data")
+    return (m)
+  }
+  data <- x$get ()
+  m<- solve (matrix)
+  x$setmatrix (m)
+  m
 }
